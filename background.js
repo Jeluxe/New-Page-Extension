847287c1592f5d638af7d05eab8bdd190b032f99
Jeluxe <59278268+Jeluxe@users.noreply.github.com>
2025-11-03 22:29:49 +0200

    Added folder selection dropdown in popup, in order to create new card inside existing folder

--- background.js
+++ background.js
@@ -1,14 +1,19 @@
 let titleInput = document.getElementById("title-input");
 let urlInput = document.getElementById("url-input");
-let selectFolderDropdown = document.getElementById("select-folder");
+let folderSelectionDropdown = document.getElementById("select-folder");
 let button = document.getElementById("submit");
 let exportBtn = document.getElementById("export");
 let tempFavIconUrl;
 let cardList;
 
+const maxCardsInList = 20;
+const maxCardsInFolder = 8;
+const newTabUrl = "edge://newtab/";
+const cardDefaultColor = "rgba(58, 58, 58, 0.425)";
+
 titleInput?.addEventListener("keyup", (e) => (titleInput.value = e.target.value));
 urlInput?.addEventListener("keyup", (e) => (urlInput.value = e.target.value));
-exportBtn.addEventListener("click", () => downloadFile(true));
+exportBtn?.addEventListener("click", () => downloadFile(true));
 
 document.addEventListener("DOMContentLoaded", async () => {
   const { title, url, favIconUrl } = await getCurrentTab();
@@ -17,11 +22,10 @@ document.addEventListener("DOMContentLoaded", async () => {
   urlInput.value = url;
   cardList = fetchData("cards");
   createOptions(cardList)
-
   const newTabs = await getNewTab();
 
   button.addEventListener("click", async (e) => {
-    if (cardList.length < 20 || cardList[selectFolderDropdown.value].cards.length < 8) {
+    if (cardList.length < maxCardsInList || cardList[folderSelectionDropdown.value].cards.length < maxCardsInFolder) {
       const convertedImg = await convertImgUrlToBase64(favIconUrl);
       await updateCards(titleInput.value, urlInput.value, convertedImg);
       if (newTabs && typeof newTabs === "object") {
@@ -39,14 +43,14 @@ document.addEventListener("DOMContentLoaded", async () => {
 async function getCurrentTab() {
   let queryOptions = { active: true, currentWindow: true };
   let [tab] = await chrome.tabs.query(queryOptions);
-  if (tab.url === "edge://newtab/") {
-    return { title: "", url: "", favIconUrl: "" };
+  if (tab.url === newTabUrl) {
+    return { index: tab.index, title: "", url: "", favIconUrl: "" };
   }
   return tab;
 }
 
 async function getNewTab() {
-  let queryOptions = { url: "edge://newtab/" };
+  let queryOptions = { url: newTabUrl };
   let tabs = await chrome.tabs.query(queryOptions);
   return tabs;
 }
@@ -56,26 +60,30 @@ const createOptions = (cardList) => {
 
   for (let item of filteredList) {
     const newOption = createOption(item)
-    selectFolderDropdown.appendChild(newOption)
+    folderSelectionDropdown.appendChild(newOption)
   }
 }
 
 const createOption = (item) => {
   const newOption = document.createElement("option")
   newOption.setAttribute("value", item.position);
-  newOption.disabled = item.cards.length === 8;
+  newOption.disabled = item.cards.length === maxCardsInFolder;
   newOption.innerText = toCamelCase(item.name)
   return newOption
 }
 
 const updateCards = async (title, url, logo) => {
-  const selectFolderPos = Number(selectFolderDropdown.value)
+  const selectFolderPos = Number(folderSelectionDropdown.value)
   const newCardData = {
     title,
     url,
     logo,
-    color: "rgba(58, 58, 58, 0.425)",
-    position: cardList.length ? selectFolderPos ? cardList[selectFolderPos].cards.length : cardList.length : 0,
+    color: cardDefaultColor,
+    position: cardList.length ?
+      selectFolderPos ?
+        cardList[selectFolderPos].cards.length :
+        cardList.length :
+      0,
   }
   if (selectFolderPos) {
     cardList = cardList.map(card => {
@@ -85,9 +93,8 @@ const updateCards = async (title, url, logo) => {
           ...card,
           cards: card.cards
         }
-      } else {
-        return card
       }
+      return card
     })
   } else {
     cardList.push(newCardData);
